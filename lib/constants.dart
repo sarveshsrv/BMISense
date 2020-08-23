da5daa0a4a775c8c2f4022ef01476ec269b60724
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-08-23 09:02:15 +0530

    Text Styles added

--- lib/constants.dart
+++ lib/constants.dart
@@ -25,17 +25,17 @@ const kTitleTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
-// const kResultTextStyle = TextStyle(
-//   color: Color(0xFF24D876),
-//   fontSize: 22.0,
-//   fontWeight: FontWeight.bold,
-// );
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
 
-// const kBMITextStyle = TextStyle(
-//   fontSize: 100.0,
-//   fontWeight: FontWeight.bold,
-// );
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
 
-// const kBodyTextStyle = TextStyle(
-//   fontSize: 22.0,
-// );
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
